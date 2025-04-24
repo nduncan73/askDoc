158af3bb0444551954bee2d57670c9eebc77938a
nduncan73 <derek.duncan@trojans.dsu.edu>
2025-04-24 08:16:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,14 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
-# from langchain.llms import OpenAI
-from langchain.chat_models import ChatOpenAI # testing updated code
+from langchain.llms import OpenAI
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import Chroma
+from langchain.vectorstores.chroma import Chroma
 from langchain.chains import RetrievalQA
+import chromadb
+import chromadb.config
 
 def generate_response(uploaded_file, openai_api_key, query_text):
     # Load document if file is uploaded
@@ -42,7 +46,7 @@ with st.form('myform', clear_on_submit=True):
         with st.spinner('Calculating...'):
             response = generate_response(uploaded_file, openai_api_key, query_text)
             result.append(response)
-            del openai_api_key
+            #del openai_api_key
 
 if len(result):
     st.info(response)
